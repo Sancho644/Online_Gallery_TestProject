31c7f63ef987909306ef95f72fed58d73b89716a
Alexandr <rfst@mail.ru>
2026-02-13 12:45:17 +0700

    create close popup button

---
+++ Assets/Scripts/UI/Popups/Buttons/CloseButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Popups.Buttons
+{
+    public class CloseButton : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+        [SerializeField] private AbstractPopup popup;
+
+        private void Awake()
+        {
+            button.onClick.AddListener(ClosePopup);
+        }
+
+        private void OnDestroy()
+        {
+            button.onClick.RemoveListener(ClosePopup);
+        }
+
+        private void ClosePopup()
+        {
+            if (popup != null)
+            {
+                popup.ClosePopup();
+            }
+        }
+    }
+}
